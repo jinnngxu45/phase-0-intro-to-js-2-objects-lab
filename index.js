eac11e07004a6c2489a5cae3e76d1434a140ff22
“Jing <+>
2021-11-30 14:00:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+let employee = {
+    streetAddress: '',
+    name: ''
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, Sam) {
+    employee[name]= Sam
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(newEmployee, name, Sam) {
+    newEmployee[name]= Sam
+    return newEmployee;
+};
